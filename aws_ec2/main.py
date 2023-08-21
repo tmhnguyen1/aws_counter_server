66a311c1bf830259da9d837eb5231d1baf5e4b83
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-21 14:37:23 +0800

    add login, user table

--- aws_ec2/main.py
+++ aws_ec2/main.py
@@ -1,12 +1,18 @@
 from datetime import datetime, date
 import json
 from collections import deque
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, abort, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from sqlalchemy import create_engine, func
 import pickle
 import os
+
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import jwt
+
 
 label_list = ['1. Harsh acceleration',\
             '2. Harsh deceleration',\
@@ -14,14 +20,34 @@ label_list = ['1. Harsh acceleration',\
             '5. Tailgating',\
             # '6. Phone handling',\
             '7. Lane switch']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 server = Flask(__name__)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
-server.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+server.config['SECRET_KEY'] = SECRET_KEY
 server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db/labels.db')
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(server)
+
+login_manager = LoginManager()
+login_manager.init_app(server)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# counter_label: admin_only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*arg, **kwargs):
+        if current_user.id not in [1,2]:
+            return abort(403)
+        else:
+            return f(*arg, **kwargs)
+    return decorated_function
+
 
 ## CREATE TABLE IN DB
 class Counter(db.Model):
@@ -33,6 +59,14 @@ class Counter(db.Model):
     date = db.Column(db.Date, default=date.today)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_name = db.Column(db.String(100))
+    
+    
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))  # hashed password
+
 
 # # Line below only required once, when creating DB
 # def create_initial_records():
@@ -40,11 +74,55 @@ class Counter(db.Model):
 #     db.session.bulk_save_objects(counters)
 #     db.session.commit()    
 
-# with server.app_context():
-#     db.create_all()
-#     create_initial_records()
+# def create_users(user_list):
+#     for record in user_list:
+#         print(record)
+#         username, password = record
+#         hashed_salty_password = generate_password_hash(password,
+#                                                         salt_length=8)
+#         new_user = User(password=hashed_salty_password,
+#                         username=username) 
+#         db.session.add(new_user)
+#         db.session.commit()
+
+with server.app_context():
+    db.create_all()
+    # create_initial_records()
+    # create_users([])
+    # meta = db.metadata
+    # meta.reflect(bind=db.engine)
+    # print(meta.tables.keys())
+
+
+@server.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('counter', user_name=username))
+            else:
+                flash('Invalid Password')
+                return redirect(url_for('login'))
+        else:
+            flash('Invalid Username')
+            return redirect(url_for('login'))
+    return render_template("login.html")
+
+
+@server.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @server.route('/counter/<user_name>', methods=['GET', 'POST'])
+@login_required
 def counter(user_name):
     counters = db.session.query(Counter.label_no, Counter.label_desc, Counter.count_val, func.max(Counter.timestamp), Counter.date).group_by(Counter.label_no).all()
     if request.method == 'POST':
@@ -66,11 +144,44 @@ def counter(user_name):
                                 user_name=user_name)
             db.session.add(timestamp)
             db.session.commit()
-    return render_template('counter.html', counters=counters, user_name=user_name)  
+    return render_template('counter.html', counters=counters, user_name=user_name, logged_in=current_user.is_authenticated)  
+
+
+
+@server.route('/download_counter/<date_to_get>')
+@admin_only
+def download_counter(date_to_get):
+    # date_to_get: "YYYY-mm-dd"
+    sql_engine = create_engine(os.path.join('sqlite:///' + os.path.join(base_dir, 'db/labels.db')), echo=False)
+    results = pd.read_sql_query("SELECT * FROM counter", sql_engine)
+    if date_to_get != 'all':
+        results = results[results.date == date_to_get]
+    results.to_csv(f'./static/files/counter_data/counter_{date_to_get}.csv', index=False)
+    return send_from_directory(directory='static', path=f'files/counter_data/counter_{date_to_get}.csv')
+
+
+######################################
+#######  Sensor logger ###############
+# Decorator function to check for authentication
+def requires_authentication(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'Authorization' not in request.headers:
+            return jsonify({'message': 'Authorization required'}), 401
+        auth_header = request.headers.get('Authorization')
+        try:
+            # Extract the token from the header
+            token = auth_header.split(' ')[1]  # Assuming the token is sent as "Bearer <token>"
+            decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except IndexError:
+            return jsonify({'message': 'Invalid Authorization header format'}), 401        
+        return f(decoded_token, *args, **kwargs)
+    return decorated
 
 
 @server.route("/data", methods=["POST"])
-def data():  # listens to the data streamed from the sensor logger
+@requires_authentication
+def data(decoded_token):  # listens to the data streamed from the sensor logger
 	if str(request.method) == "POST":
 		data = json.loads(request.data)
 		print(f'received data: {data["payload"]}')
@@ -83,17 +194,8 @@ def data():  # listens to the data streamed from the sensor logger
 			pickle.dump(data['payload'], f)		
 	return "success"
 
-@server.route('/download_counter/<date_to_get>')
-def download_counter(date_to_get):
-    # date_to_get: "YYYY-mm-dd"
-    sql_engine = create_engine(os.path.join('sqlite:///' + os.path.join(base_dir, 'db/labels.db')), echo=False)
-    results = pd.read_sql_query("SELECT * FROM counter", sql_engine)
-    if date_to_get != 'all':
-        results = results[results.date == date_to_get]
-    results.to_csv(f'./static/files/counter_data/counter_{date_to_get}.csv', index=False)
-    return send_from_directory(directory='static', path=f'files/counter_data/counter_{date_to_get}.csv')
-
 
 if __name__ == "__main__":
 	# run the web server
-	server.run(port=8000, host="0.0.0.0", debug=True)
+    print(SECRET_KEY)
+    server.run(port=8000, host="0.0.0.0", debug=True)
