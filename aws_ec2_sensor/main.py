4577fc814afd4bf91a37e360f81a9f983e870bc3
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-24 17:03:58 +0800

    turn off print

--- aws_ec2_sensor/main.py
+++ aws_ec2_sensor/main.py
@@ -39,9 +39,9 @@ def requires_authentication(f):
 def data(device_id):  # listens to the data streamed from the sensor logger
 	if str(request.method) == "POST":
 		data = json.loads(request.data)
-		print(f'received data: {data["payload"]}')
 		timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 		date_to_get = datetime.now().strftime('%Y-%m-%d')
+		print(f'received data: timestamp')
 		if not os.path.exists(base_dir + f'/data/{device_id}/{date_to_get}'):
 			os.makedirs(base_dir + f'/data/{device_id}/{date_to_get}')
 		filename = base_dir + f'/data/{device_id}/{date_to_get}/{timestamp}.pkl'
