1d0399552959d4d62980c5c17c34153f70eea29b
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-24 09:25:02 +0800

    add all dbs/tables

--- aws_ec2_sensor/main.py
+++ aws_ec2_sensor/main.py
@@ -30,13 +30,13 @@ server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'gyroscope.db')
 
 server.config['SQLALCHEMY_BINDS'] = {
-    'db2': 'sqlite:///' + os.path.join(base_dir, f'db/gyroscopeuncalibrated.db')#,
-    # 'db3': 'sqlite:///' + os.path.join(base_dir, f'db/accelerometer.db'),
-    # 'db4': 'sqlite:///' + os.path.join(base_dir, f'db/accelerometeruncalibrated.db'),
-    # 'db5': 'sqlite:///' + os.path.join(base_dir, f'db/location.db'),
-    # 'db6': 'sqlite:///' + os.path.join(base_dir, f'db/barometer.db'),
-    # 'db7': 'sqlite:///' + os.path.join(base_dir, f'db/gravity.db'),
-    # 'db8': 'sqlite:///' + os.path.join(base_dir, f'db/orientation.db')
+    'db2': 'sqlite:///' + os.path.join(base_dir, f'db/gyroscopeuncalibrated.db'),
+    'db3': 'sqlite:///' + os.path.join(base_dir, f'db/accelerometer.db'),
+    'db4': 'sqlite:///' + os.path.join(base_dir, f'db/accelerometeruncalibrated.db'),
+    'db5': 'sqlite:///' + os.path.join(base_dir, f'db/location.db'),
+    'db6': 'sqlite:///' + os.path.join(base_dir, f'db/barometer.db'),
+    'db7': 'sqlite:///' + os.path.join(base_dir, f'db/gravity.db'),
+    'db8': 'sqlite:///' + os.path.join(base_dir, f'db/orientation.db')
 }
 
 db = SQLAlchemy()
@@ -65,10 +65,97 @@ class GyroscopeUncalibrated(db.Model):
     date = db.Column(db.Date, default=date.today)
     username = db.Column(db.String(100))    
     
+    
+class Accelerometer(db.Model):
+    __bind_key__ = 'db3'
+    __tablename__ = "accelerometer"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
+    z = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100))      
+    
 
+class AccelerometerUncalibrated(db.Model):
+    __bind_key__ = 'db4'
+    __tablename__ = "accelerometeruncalibrated"
+    id = db.Column(db.Integer, primary_key=True)    
+    time = db.Column(db.Integer)
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
+    z = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100)) 
+    
+    
+class Location(db.Model):
+    __bind_key__ = 'db5'
+    __tablename__ = "location"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    bearingAccuracy = db.Column(db.Float)
+    speedAccuracy = db.Column(db.Float)
+    verticalAccuracy = db.Column(db.Float)
+    horizontalAccuracy = db.Column(db.Float)
+    speed = db.Column(db.Float)
+    bearing = db.Column(db.Float)
+    altitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100))  
+    
+    
+class Barometer(db.Model):
+    __bind_key__ = 'db6'
+    __tablename__ = "barometer"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    relativeAltitude = db.Column(db.Float)
+    pressure = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100))  
+        
+        
+class Gravity(db.Model):
+    __bind_key__ = 'db7'
+    __tablename__ = "gravity"
+    id = db.Column(db.Integer, primary_key=True)    
+    time = db.Column(db.Integer)
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
+    z = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100))   
+    
+    
+class Orientation(db.Model):
+    __bind_key__ = 'db8'
+    __tablename__ = "orientation"
+    id = db.Column(db.Integer, primary_key=True)    
+    time = db.Column(db.Integer)
+    qx = db.Column(db.Float)
+    qy = db.Column(db.Float)
+    qz = db.Column(db.Float)
+    qw = db.Column(db.Float)
+    roll = db.Column(db.Float)
+    pitch = db.Column(db.Float)
+    yaw = db.Column(db.Float)
+    date = db.Column(db.Date, default=date.today)
+    username = db.Column(db.String(100))    
+         
+    
 with server.app_context():
     db.create_all()
     db.create_all(bind='db2')
+    db.create_all(bind='db3')
+    db.create_all(bind='db4')
+    db.create_all(bind='db5')
+    db.create_all(bind='db6')
+    db.create_all(bind='db7')
+    db.create_all(bind='db8')
     
 ######################################
 #######  Sensor logger ###############
@@ -117,6 +204,62 @@ def data():  # listens to the data streamed from the sensor logger
                 db.session.add(new)
                 db.session.commit()
                 continue
+            if item_name == 'accelerometer':
+                new = Accelerometer(time=item_time,
+                                    x=item_values['x'],
+                                    y=item_values['y'],
+                                    z=item_values['z'],
+                                    date=datetime.fromtimestamp(item_time/10e8).date(),
+                                    username='test')
+                db.session.add(new)
+                db.session.commit()
+                continue
+            if item_name == 'accelerometeruncalibrated':
+                new = AccelerometerUncalibrated(time=item_time,
+                                                x=item_values['x'],
+                                                y=item_values['y'],
+                                                z=item_values['z'],
+                                                date=datetime.fromtimestamp(item_time/10e8).date(),
+                                                username='test')
+                db.session.add(new)
+                db.session.commit() 
+            if item_name == 'location':
+                new = Location(time=item_time,
+                                bearingAccuracy=item_values['bearingAccuracy'],
+                                speedAccuracy=item_values['speedAccuracy'],
+                                verticalAccuracy=item_values['verticalAccuracy'],
+                                horizontalAccuracy=item_values['horizontalAccuracy'],
+                                speed=item_values['speed'],
+                                bearing=item_values['bearing'],
+                                altitude=item_values['altitude'],
+                                longitude=item_values['longitude'],
+                                latitude=item_values['latitude'],
+                                date=datetime.fromtimestamp(item_time/10e8).date(),
+                                username='test')
+                db.session.add(new)
+                db.session.commit()
+            if item_name == 'gravity':
+                new = Gravity(time=item_time,
+                                x=item_values['x'],
+                                y=item_values['y'],
+                                z=item_values['z'],
+                                date=datetime.fromtimestamp(item_time/10e8).date(),
+                                username='test')
+                db.session.add(new)
+                db.session.commit()
+            if item_name == 'orientation':
+                new = Orientation(time=item_time,
+                                    qz=item_values['qz'],
+                                    qy=item_values['qy'],
+                                    qx=item_values['qx'],
+                                    qw=item_values['qw'],
+                                    roll=item_values['roll'],
+                                    pitch=item_values['pitch'],
+                                    yaw=item_values['yaw'],
+                                    date=datetime.fromtimestamp(item_time/10e8).date(),
+                                    username='test')
+                db.session.add(new)
+                db.session.commit()                                                    
     return 'success'
 
 
@@ -134,20 +277,18 @@ def download_data(date_to_get):
     
     if not os.path.exists(f'static/{date_to_get}'):
         os.mkdir(f'static/{date_to_get}')
+
+    TABLES = [Gyroscope, GyroscopeUncalibrated, Accelerometer, AccelerometerUncalibrated, Location, Barometer, Gravity, Orientation]
+    TABLE_NAMES = ['Gyroscope', 'GyroscopeUncalibrated', 'Accelerometer', 'AccelerometerUncalibrated', 'Location', 'Barometer', 'Gravity', 'Orientation']
     
-    data = db.session.query(Gyroscope).all()
-    results = pd.DataFrame([(d.id, d.time, d.x, d.y, d.z, d.date, d.username) for d in data],
-                           columns=['id', 'time', 'x', 'y', 'z', 'date', 'username'])
-    if date_to_get != 'all':
-        results = results[results.date == date_to_get]
-    results.to_csv(f'./static/{date_to_get}/Gyroscope.csv', index=False)
-    
-    data = db.session.query(GyroscopeUncalibrated).all()
-    results = pd.DataFrame([(d.id, d.time, d.x, d.y, d.z, d.date, d.username) for d in data],
-                           columns=['id', 'time', 'x', 'y', 'z', 'date', 'username'])
-    if date_to_get != 'all':
-        results = results[results.date == date_to_get]
-    results.to_csv(f'./static/{date_to_get}/GyroscopeUncalibrated.csv', index=False)
+    for table, table_name in zip(TABLES, TABLE_NAMES):
+        data = db.session.query(table).all()
+        columns = table.__table__.columns.keys()
+
+        results = pd.DataFrame([tuple(getattr(d, column) for column in columns) for d in data], columns=columns)
+        if date_to_get != 'all':
+            results = results[results.date == date_to_get]
+        results.to_csv(f'./static/{date_to_get}/{table_name}.csv', index=False)
     
     zip_folder(f'static/{date_to_get}', f'static/{date_to_get}.zip')  
     
