30b7a7268f6bdb1a3c1754c3748f8040dac09e13
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-25 09:55:44 +0800

    log cron jobs

--- aws_ec2_sensor/sync.sh
+++ aws_ec2_sensor/sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "$(date) Log entry" >> /home/ec2-user/learning/aws_ec2_sensor/cronlog.log
 aws s3 sync /home/ec2-user/learning/aws_ec2_sensor/data/ s3://lalamove-apas/sensor_logger
