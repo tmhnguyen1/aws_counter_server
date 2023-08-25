e62debebbb241ccc4f6fd642e285d6962a45cd01
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-25 18:01:59 +0800

    fix bug

--- aws_ec2_sensor/data_convert.py
+++ aws_ec2_sensor/data_convert.py
@@ -61,18 +61,20 @@ def extract_sensor_data(input_folder, csv_output_path, pkl_output_path):
             shutil.move(pkl_file, os.path.join(pkl_output_path, os.path.basename(pkl_file)))
         except (EOFError, pickle.UnpicklingError):
             print(f"Failed to load data from {pkl_file}")
-
-    os.makedirs(csv_output_path+f'/{pkl_file}', exist_ok=True)
+            
+    last_file = pkl_file.split("/")[-1].split(".")[0][-19:]
+    print('last_file', last_file)
+    os.makedirs(csv_output_path + f'/{last_file}', exist_ok=True)
     for sensor_name, sensor_values in sensor_data.items():
         sensor_df = pd.DataFrame(sensor_values)
-        sensor_df.to_csv(os.path.join(csv_output_path, f'/{pkl_file}/{sensor_name.capitalize()}.csv'), index=False)
+        sensor_df.to_csv(csv_output_path + f'/{last_file}/{sensor_name.capitalize()}.csv', index=False)
 
     return sensor_data
 
 
 if __name__ == '__main__':
-    parent_input_folder = '/home/ec2-user/learning/aws_ec2_sensor/data_raw/'
-    # parent_input_folder = 'C:/Users/tmhnguyen/Documents/lalamove/lalamove/data/data_raw/'
+    # parent_input_folder = '/home/ec2-user/learning/aws_ec2_sensor/data_raw/'
+    parent_input_folder = 'C:/Users/tmhnguyen/Documents/lalamove/lalamove/data/data_raw/'
     
     for root, dirs, files in os.walk(parent_input_folder):
         # print('root', root)
@@ -80,6 +82,6 @@ if __name__ == '__main__':
         if len(files):
             csv_output_path = root.replace('data_raw', 'data_csv')
             pkl_output_path = root.replace('data_raw', 'data_pkl')
-            # print(' csv', csv_output_path)
-            # print(' pkl', pkl_output_path)
+            print(' csv', csv_output_path)
+            print(' pkl', pkl_output_path)
             extract_sensor_data(root, csv_output_path, pkl_output_path)
