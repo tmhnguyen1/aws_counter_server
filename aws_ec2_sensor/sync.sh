833c17bbab70c20fa2ca88dbf286c75fdddbb828
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-25 17:28:26 +0800

    update sync.sh

--- aws_ec2_sensor/sync.sh
+++ aws_ec2_sensor/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$(date) Log entry" >> /home/ec2-user/learning/aws_ec2_sensor/cronlog.log
+echo "$(date) Sync entry" >> /home/ec2-user/learning/aws_ec2_sensor/cronlog.log
 aws s3 sync /home/ec2-user/learning/aws_ec2_sensor/data_csv/ s3://lalamove-apas/sensor_logger
