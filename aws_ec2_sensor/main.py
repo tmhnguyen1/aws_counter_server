fa253048de03068e8e89072535535932755cd9d5
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-08-25 11:35:45 +0800

    remove authentication

--- aws_ec2_sensor/main.py
+++ aws_ec2_sensor/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date, timedelta
 import json
 from collections import deque
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, abort, jsonify, session
+from flask import Flask, request
 import pandas as pd
 import pickle
 import os
@@ -16,22 +16,6 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 server.config['SECRET_KEY'] = SECRET_KEY
 server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db/labels.db')
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-def requires_authentication(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if 'Authorization' not in request.headers:
-            return jsonify({'message': 'Authorization required'}), 401
-        auth_header = request.headers.get('Authorization')
-        try:
-            # Extract the token from the header
-            token = auth_header.split(' ')[1]  # Assuming the token is sent as "Bearer <token>"
-            decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        except IndexError:
-            return jsonify({'message': 'Invalid Authorization header format'}), 401        
-        return f(decoded_token, *args, **kwargs)
-    return decorated
 
 
 @server.route("/data/<device_id>", methods=["POST"])
@@ -51,9 +35,5 @@ def data(device_id):  # listens to the data streamed from the sensor logger
 
 
 if __name__ == "__main__":
-    import socket
-    hostname = socket.gethostname()
-    print(socket.gethostbyname(hostname))
-    
 	# run the web server
     server.run(port=8000, host="0.0.0.0", debug=True)
