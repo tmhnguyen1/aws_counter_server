34ce499337e24c3ce1d1f5bc507d03a1f3484fa1
tmhnguyen1 <tmhnguyen@hkpc.org>
2023-12-27 17:03:37 +0800

    fix path to share data

--- aws_sync/sync.sh
+++ aws_sync/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 AWS_CLI_PATH="/usr/local/bin/aws"
-echo "$(date) Log entry" >> /app/counter_server/db/cronlog.log
-$AWS_CLI_PATH s3 sync /app/counter_server/db/ s3://lalamove-apas/counter/test_counter --region ap-east-1
+echo "$(date) Log entry" >> /app/db/cronlog.log
+$AWS_CLI_PATH s3 sync /app/db/ s3://lalamove-apas/counter/test_counter --region ap-east-1
