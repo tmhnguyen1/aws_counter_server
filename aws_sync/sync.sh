e88ebd809446e76f7a56b712f0b74e460902a757
Thi Minh Hoa NGUYEN <tmhnguyen@hkpc.org>
2024-01-09 15:20:50 +0800

    fix to sync only db file

--- aws_sync/sync.sh
+++ aws_sync/sync.sh
@@ -2,4 +2,4 @@
 
 AWS_CLI_PATH="/usr/local/bin/aws"
 echo "$(date) Log entry" >> /app/db/cronlog.log
-$AWS_CLI_PATH s3 sync --exclude "*" --include "db/label*" /app/db s3://sengital-apas/counter --region ap-east-1
+$AWS_CLI_PATH s3 sync /app/db s3://sengital-apas/counter --region ap-east-1 --exclude "*" --include "label*"
