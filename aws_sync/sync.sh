4eb8fdd697a22f4e443749e79d24318820f41542
Thi Minh Hoa NGUYEN <tmhnguyen@hkpc.org>
2024-01-09 14:14:26 +0800

    change s3 path

--- aws_sync/sync.sh
+++ aws_sync/sync.sh
@@ -2,4 +2,4 @@
 
 AWS_CLI_PATH="/usr/local/bin/aws"
 echo "$(date) Log entry" >> /app/db/cronlog.log
-$AWS_CLI_PATH s3 sync /app/db/ s3://sengital-apas/counter/counter_1 --region ap-east-1
+$AWS_CLI_PATH s3 sync /app/db/ s3://sengital-apas/counter --region ap-east-1
